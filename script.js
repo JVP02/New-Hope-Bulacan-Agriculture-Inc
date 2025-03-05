2087bdff8a516831b3c1804878a61c473d4f9c0a
John Vincent Peralta <87950013+JVP02@users.noreply.github.com>
2025-03-05 14:17:20 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -42,3 +42,14 @@ setInterval(() => {
 }, 5000);
 
 
+
+// Scroll to Section JS ===========================================================================================================
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    const offset = 125; // Adjust this value based on your header height
+
+    if (section) {
+        const sectionPosition = section.getBoundingClientRect().top + window.scrollY - offset;
+        window.scrollTo({ top: sectionPosition, behavior: 'smooth' });
+    }
+}
