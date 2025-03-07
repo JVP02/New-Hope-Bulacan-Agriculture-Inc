0164817b87c1a98112ff802b37d27f776022255c
John Vincent Peralta <87950013+JVP02@users.noreply.github.com>
2025-03-07 09:58:06 +0800

    Delete contact.js

--- contact.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const phoneInput = document.querySelector('input[name="phone"]');
-    phoneInput.addEventListener('input', () => {
-        phoneInput.value = phoneInput.value.replace(/\D/g, ''); // Only allow numbers
-    });
-});
